9251bbbbd8a028bea75ccbf81858882d4f05c1e1
Donz <donnygapulao@gmail.com>
2022-10-11 16:49:17 +0800

    added title default props

--- components/shared/AppNavigation.js
+++ components/shared/AppNavigation.js
@@ -1,14 +1,14 @@
 import { Box, HStack, IconButton, Icon, Text } from 'native-base';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default () => (
+const AppNavigation = (props) => (
     <>
         <Box safeAreaTop bg="primary.500"/>
         <HStack bg="primary.500" px="1" pt="0" pb="2" justifyContent="space-between" w="100%">
            <HStack alignItems="center">
                 <IconButton icon={<Icon as={Ionicons} name='md-menu' size='5'/>} _icon={{color:"white"}}  />
                 <Text color="white" fontSize="20" fontWeight="bold">
-                    Tile Solutions
+                    {props.title}
                 </Text>
             </HStack>
             <HStack>
@@ -17,3 +17,12 @@ export default () => (
         </HStack>
     </>
 )
+
+AppNavigation.defaultProps = {
+    title: 'Tile Solutions'
+}
+
+export default AppNavigation;
+
+
+
