9bf7567b03471a42a0463cc0b5f747562f74b4ae
Donz <donnygapulao@gmail.com>
2022-10-11 17:28:09 +0800

    added initial handler

--- components/shared/AppNavigation.js
+++ components/shared/AppNavigation.js
@@ -1,23 +1,40 @@
+import React from 'react';
 import { Box, HStack, IconButton, Icon, Text } from 'native-base';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-const AppNavigation = (props) => (
-    <>
-        <Box safeAreaTop bg="primary.500"/>
-        <HStack bg="primary.500" px="1" pt="0" pb="2" justifyContent="space-between" w="100%">
-           <HStack alignItems="center">
-                <IconButton icon={<Icon as={Ionicons} name='md-menu' size='5'/>} _icon={{color:"white"}}  />
-                <Text color="white" fontSize="20" fontWeight="bold">
-                    {props.title}
-                </Text>
-            </HStack>
-            <HStack>
-                <IconButton icon={<Icon as={Ionicons} name='md-ellipsis-vertical' size='5'/>} _icon={{color:"white"}}  />
-            </HStack>
-        </HStack>
-    </>
-)
+const AppNavigation = (props) => {
+    const [openDrawer, setOpenDrawer] = React.useState(false);
+    const [openMoreMenu, setMoreMenu] = React.useState(false);
 
+    const handleToggleDrawer = () => {
+        setOpenDrawer(!openDrawer);
+        console.warn(openDrawer);
+    }
+
+    const handleOpenMoreMenu= () => {
+        setMoreMenu(!openMoreMenu);
+        console.warn(openMoreMenu);
+    }
+
+    return (
+        <>
+            <Box safeAreaTop bg="primary.500"/>
+            <HStack bg="primary.500" px="1" pt="0" pb="2" justifyContent="space-between" w="100%">
+                <HStack alignItems="center">
+                    <IconButton icon={<Icon as={Ionicons} name='md-menu' size='5'/>} _icon={{color: "white"}}
+                                onPress={handleToggleDrawer}/>
+                    <Text color="white" fontSize="20" fontWeight="bold">
+                        {props.title}
+                    </Text>
+                </HStack>
+                <HStack>
+                    <IconButton onPress={handleOpenMoreMenu} icon={<Icon as={Ionicons} name='md-ellipsis-vertical' size='5'/>}
+                                _icon={{color: "white"}}/>
+                </HStack>
+            </HStack>
+        </>
+    )
+}
 AppNavigation.defaultProps = {
     title: 'Tile Solutions'
 }
