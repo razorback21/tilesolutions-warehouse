3e365acf53f988c54fc09181246d3413dac2d11b
Donz <donnygapulao@gmail.com>
2022-10-18 18:36:42 +0800

    button text to regular

--- components/shared/WhiteButton.js
+++ components/shared/WhiteButton.js
@@ -8,7 +8,7 @@ export default (props) => {
         <>
             <Button onPress={props.handler} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="xl" style={{color:"text.700"}}/>} mb={props.mb} mt={props.mt}>
                 <Stack direction="row" >
-                    <Text fontWeight="bold" w="80%" fontSize="16">{props.title}</Text>
+                    <Text w="80%" fontSize="16">{props.title}</Text>
                     <Icon position="absolute" right="-65" top="0.5" size="md" as={MaterialIcons} name="arrow-forward-ios" color="primary.600"/>
                 </Stack>
             </Button>
