59e1e316c8b733aa0a2dddb415de9fd9ea96e08e
Donz <donnygapulao@gmail.com>
2022-10-18 17:52:34 +0800

    order picking screen initial code

--- components/screens/OrderPickng.js
+++ components/screens/OrderPickng.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { StyleSheet } from 'react-native';
-import {Box, VStack, Text, Heading, Input, Button, Center} from "native-base";
+import {Box, VStack, Text, Heading, Input, Button, Center, Icon, HStack, Stack, Divider} from "native-base";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import AppNavigation from "../shared/AppNavigation";
+import WhiteButton from "../shared/WhiteButton";
 
 
 
 export default () => {
+
+    const scanNFCTag = () => {
+        console.warn('scanning NFC tag');
+    }
+
+    const scanQRCode = () => {
+        console.warn('scanning QR Code');
+    }
+
+    const orderReceived = () => {
+        console.warn('Goto order received');
+    }
 
     return (
         <>
@@ -21,6 +35,12 @@ export default () => {
                 <Center>
                     <Heading ize="lg" mb="5" color="text.500" my="3">or</Heading>
                 </Center>
+                <VStack>
+                    <WhiteButton title="Scan NFC Tag" icon="nfc" mb="2" handler={scanNFCTag}/>
+                    <WhiteButton title="Scan QR code" icon="qr-code-scanner"handler={scanQRCode}/>
+                </VStack>
+                <Divider my="6"/>
+                <WhiteButton title="Order Received" icon="call-received" mb="2" handler={orderReceived}/>
             </Box>
         </>
     )
