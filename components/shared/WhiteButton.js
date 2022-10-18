3e8c8474cf1cf052f4526e694fd81c2fefc074f6
Donz <donnygapulao@gmail.com>
2022-10-18 17:52:02 +0800

    updated white button component

--- components/shared/WhiteButton.js
+++ components/shared/WhiteButton.js
@@ -6,7 +6,7 @@ import React from "react";
 export default (props) => {
     return (
         <>
-            <Button onPress={props.handler} variant="white" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="lg" style={{color:"text.700"}}/>} mb={props.mb} mt={props.mt}>
+            <Button onPress={props.handler} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="lg" style={{color:"text.700"}}/>} mb={props.mb} mt={props.mt}>
                 <Stack direction="row" >
                     <Text fontWeight="bold" w="80%">{props.title}</Text>
                     <Icon position="absolute" right="-70" size="sm" as={MaterialIcons} name="arrow-forward-ios" color="primary.600"/>
