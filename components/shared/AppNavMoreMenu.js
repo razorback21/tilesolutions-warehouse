156e6aad9c2885a1f2d1b633abe1078720de8b53
Donz <donnygapulao@gmail.com>
2022-10-18 11:28:47 +0800

    added appnav more menu

---
+++ components/shared/AppNavMoreMenu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Box, Menu, Pressable, Text, Button, Icon, IconButton} from 'native-base';
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+
+const AppNavMoreMenu = () => {
+    const logout = () => {
+        console.warn('log out')
+    }
+
+    return (<Box w="5%" alignItems="center">
+                <Menu on w="150" defaultIsOpen={false} trigger={triggerProps => {
+                       return <IconButton {...triggerProps} icon={<Icon as={Ionicons} name='md-ellipsis-vertical' size='5'/>}
+                                         _icon={{color: "white"}}/>;
+                }}>
+                    <Menu.Item onPress={logout}>Log out</Menu.Item>
+                </Menu>
+            </Box>
+    )
+}
+
+export default AppNavMoreMenu;
