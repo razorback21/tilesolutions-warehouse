4b7067da01fc982024d6462af62e38e11a5ddcca
Donz <donnygapulao@gmail.com>
2022-10-19 18:31:10 +0800

    added app style

---
+++ AppStyles.js
@@ -0,0 +1,34 @@
+const AppStyles = {
+    topContainer: {
+        flex: 1,
+        backgroundColor: "#E7E7E9",
+        paddingTop: 35,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingBottom: 15,
+    },
+
+    topContainerNoFlex: {
+        backgroundColor: "#E7E7E9",
+        paddingTop: 35,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingBottom: 15
+    },
+
+    contentContainer: {
+        backgroundColor: "#FFFFFF",
+        flex:1,
+        paddingTop: 20,
+        paddingHorizontal: 10,
+        paddingBottom:10
+    },
+
+    innerBox: {
+        flex:1,
+        padding:8
+    }
+}
+
+
+export default AppStyles;
