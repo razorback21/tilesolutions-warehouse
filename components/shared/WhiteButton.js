0673fe9dc1cb247b7db1a8c132dce90729341c38
Donz <donnygapulao@gmail.com>
2022-10-19 12:30:38 +0800

    updated size

--- components/shared/WhiteButton.js
+++ components/shared/WhiteButton.js
@@ -6,10 +6,10 @@ import React from "react";
 export default (props) => {
     return (
         <>
-            <Button onPress={props.handler} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="xl" style={{color:"text.700"}}/>} mb={props.mb} mt={props.mt}>
+            <Button onPress={props.handler} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="md" style={{color:"text.700"}}/>} mb={props.mb} mt={props.mt}>
                 <Stack direction="row" >
-                    <Text w="80%" fontSize="16">{props.title}</Text>
-                    <Icon position="absolute" right="-65" top="0.5" size="md" as={MaterialIcons} name="arrow-forward-ios" color="primary.600"/>
+                    <Text w="80%" fontSize="14">{props.title}</Text>
+                    <Icon position="absolute" right="-70" top="0.5" size="md" as={MaterialIcons} name="arrow-forward-ios" color="primary.600"/>
                 </Stack>
             </Button>
         </>
