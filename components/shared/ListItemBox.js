90759e4ce40671d3dc17f2dbbb3d2fae98ddea2f
Donz <donnygapulao@gmail.com>
2022-10-19 15:40:28 +0800

    listItem initial code

---
+++ components/shared/ListItemBox.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {HStack, Stack, Icon, Flex, Pressable } from "native-base";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+const ListItemBox = (props) => {
+    return <Pressable _pressed={{
+        bg: "text.50"
+    }} bg="#FFFFFF"  mb="2" onPress={props.handler}>
+        <HStack p="4" h={props.h} alignItems="center">
+            <Stack w="95%">
+                {props.content}
+            </Stack>
+            <Stack h="100%" direction="row">
+                <Flex h="100%" direction="column" alignItems="center" justifyContent="center">
+                    <Icon size="md" as={MaterialIcons} name="arrow-forward-ios" color="primary.600"/>
+                </Flex>
+            </Stack>
+        </HStack>
+    </Pressable>
+}
+
+ListItemBox.defaultProps = {
+    h: 120
+}
+
+export default ListItemBox;
+
