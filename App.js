980405b3a52bde2e8a94740d52dce1fb91b02d05
Donz <donnygapulao@gmail.com>
2022-10-26 15:39:27 +0800

    fixed navigation

--- App.js
+++ App.js
@@ -10,23 +10,35 @@ import AppTheme from "./AppTheme";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import DashboardScreen from "./components/screens/picking/DashboardScreen";
 import AppDrawer from "./components/shared/AppDrawer";
+
+import DashboardScreen from "./components/screens/picking/DashboardScreen";
+import OrderPickngHomeScreen from "./components/screens/picking/OrderPickngHomeScreen";
 
 export default function App() {
   // extend the theme
   const theme = extendTheme(AppTheme);
 
   const Drawer = createDrawerNavigator();
+  const StackNav = createDrawerNavigator();
+
+  const DrawerNav = () => {
+      return (<Drawer.Navigator screenOptions={{
+          headerShown: false,
+      }} drawerContent={(props) => <AppDrawer {...props}/>}>
+          <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+      </Drawer.Navigator>)
+  }
 
   return (
       <NavigationContainer header={false}>
         <NativeBaseProvider theme={theme}>
-            <Drawer.Navigator initialRouteName="Dashboard" screenOptions={{
+            <StackNav.Navigator initialRouteName="Dashboard" screenOptions={{
                 headerShown: false,
-            }} drawerContent={(props) => <AppDrawer {...props}/>}>
-                <Drawer.Screen name="Dashboard" component={DashboardScreen} />
-            </Drawer.Navigator>
+            }} >
+                <StackNav.Screen name="Dashboard" component={DrawerNav} />
+                <StackNav.Screen name="Order Picking" component={OrderPickngHomeScreen} />
+            </StackNav.Navigator>
         </NativeBaseProvider>
       </NavigationContainer>
 
