365677e55f0f0e0b244b418ba54b616e81fc1537
Donz <donnygapulao@gmail.com>
2022-10-26 18:28:26 +0800

    updated style

--- components/screens/picking/PickingStepOneScreen.js
+++ components/screens/picking/PickingStepOneScreen.js
@@ -13,13 +13,13 @@ export default (props) => {
         return (
             <>
                 <Box mb="1">
-                    <Text fontWeight="700" fontSize="12">AA.911.2424   <Box h="2.5" width="2.5" bg="success.500" rounded="full" mt="0.2"></Box></Text>
+                    <Text fontWeight="700" fontSize="12" color="primary.600">AA.911.2424   <Box h="2.5" width="2.5" bg="success.500" rounded="full" mt="0.2"></Box></Text>
                 </Box>
                 <Box mb="1">
-                    <Text fontWeight="300" fontSize="12">La Marca Statuarietto Polished Rectified Glazed Porcelain 24" x 24"</Text>
+                    <Text fontWeight="400" fontSize="12" color="text.600">La Marca Statuarietto Polished Rectified Glazed Porcelain 24" x 24"</Text>
                 </Box>
                 <Box>
-                    <Text fontWeight="300" fontSize="12">Order Qty : <Text fontWeight="700">83 PC</Text></Text>
+                    <Text fontWeight="400" fontSize="12" color="text.600">Order Qty : 83 PC</Text>
                 </Box>
             </>
         );
@@ -32,7 +32,7 @@ export default (props) => {
                 <Text color="tertiary.500" fontSize="12">STEP 1</Text>
                 <Heading size="md" color="tertiary.700">Picking order for pickup</Heading>
 
-                <Text my="2" fontSize="12">
+                <Text my="2" fontSize="12" color="text.600">
                     Written by: Philiipine{"\n"}
                     Sold to: JS Renovations{"\n"}
                     For C.O: <Text fontWeight="700">000183381</Text>
