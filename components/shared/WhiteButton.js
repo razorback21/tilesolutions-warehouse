c0984ac073fb07ab5b9f79dce7af2eac827be818
Donz <donnygapulao@gmail.com>
2022-10-26 16:50:06 +0800

    replace handler with onpress

--- components/shared/WhiteButton.js
+++ components/shared/WhiteButton.js
@@ -6,7 +6,7 @@ import React from "react";
 export default (props) => {
     return (
         <>
-            <Button onPress={props.handler} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="md" color="text.500"/>} mb={props.mb} mt={props.mt}>
+            <Button onPress={props.onPress} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="md" color="text.500"/>} mb={props.mb} mt={props.mt}>
                 <Stack direction="row" >
                     <Text w="80%" fontSize="14" ml="1">{props.title}</Text>
                     <Icon position="absolute" right="-55" top="1.5" size="xs" as={MaterialIcons} name="arrow-forward-ios" color="primary.500"/>
