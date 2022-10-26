7f1d0d9dc89ef77548bc37d90f4b96a741988e08
Donz <donnygapulao@gmail.com>
2022-10-26 18:17:45 +0800

    added components to stack nav

--- App.js
+++ App.js
@@ -13,8 +13,14 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import AppDrawer from "./components/shared/AppDrawer";
 
 import DashboardScreen from "./components/screens/picking/DashboardScreen";
+//Order Picking
 import OrderPickingHomeScreen from "./components/screens/picking/OrderPickingHomeScreen";
 import OrderReceivedScreen from "./components/screens/picking/OrderReceivedScreen";
+import PickedItemInfoScreen from "./components/screens/picking/PickedItemInfoScreen";
+import PickingStepOneScreen from "./components/screens/picking/PickingStepOneScreen";
+import PickingStepTwoScreen from "./components/screens/picking/PickingStepTwoScreen";
+import PickingStepThreeScreen from "./components/screens/picking/PickingStepThreeScreen";
+import ActualPickingScreen from "./components/screens/picking/ActualPickingScreen";
 
 export default function App() {
   // extend the theme
@@ -31,6 +37,13 @@ export default function App() {
                 <StackNav.Screen name="Dashboard" component={DashboardScreen} />
                 <StackNav.Screen name="Order Picking" component={OrderPickingHomeScreen} />
                 <StackNav.Screen name="Order Received" component={OrderReceivedScreen} />
+
+
+                <StackNav.Screen name="Picking Step One" component={PickingStepOneScreen} />
+                <StackNav.Screen name="Picking Step Two" component={PickingStepTwoScreen} />
+                <StackNav.Screen name="Picking Step Three" component={PickingStepThreeScreen} />
+                <StackNav.Screen name="Actual Picking" component={ActualPickingScreen} />
+                <StackNav.Screen name="Picked Item Information" component={PickedItemInfoScreen} />
             </StackNav.Navigator>
         </NativeBaseProvider>
       </NavigationContainer>
