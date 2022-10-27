46dccb233cc80288ea32e829f1c2708a14c58f83
Donz <donnygapulao@gmail.com>
2022-10-27 14:18:07 +0800

    updated style to match t heme

--- components/screens/picking/ActualPickingScreen.js
+++ components/screens/picking/ActualPickingScreen.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { StyleSheet } from 'react-native';
 import {Box, Text, Heading, ScrollView, HStack, Center, Flex, Input, Button} from "native-base";
-import AppNavigation from "../../shared/AppNavigation";
 import AppStyles  from "../../../AppStyles";
 import AppBackNavigation from "../../shared/AppBackNavigation";
+import TileInfoBox from "../../shared/TileInfoBox";
 
 
 export default (props) => {
 
     const ActualPick = (props) => {
         return (
-            <Box px="2" pt="2" pb="3" bg="#D9D9D9" mb="2" rounded="4" shadow="2">
+            <Box px="2" pt="2" pb="3" bg="text.50" mb="2" rounded="4" shadow="2">
                     <Center>
                         <Heading size="md" color="tertiary.700">{props.uom}</Heading>
                     </Center>
@@ -48,26 +48,17 @@ export default (props) => {
                     </Box>
 
                     <HStack justifyContent="center" space="5" mt="4">
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">83</Text>
-                            <Text color="tertiary.700" fontWeight="700">Ordered</Text>
-                        </Box>
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">83</Text>
-                            <Text color="tertiary.700" fontWeight="700">Remaining</Text>
-                        </Box>
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">PC</Text>
-                            <Text color="tertiary.700" fontWeight="700">UoM</Text>
-                        </Box>
+                        <TileInfoBox title="83" subTitle="Ordered"/>
+                        <TileInfoBox title="83" subTitle="Remaining"/>
+                        <TileInfoBox title="PC" subTitle="UoM"/>
                     </HStack>
 
                     <Text fontSize="11" fontWeight="700" my="4" color="text.700">CONVERTION TABLE</Text>
 
-                    <Box rounded="4" py="2" alignItems="center" justifyContent="center"  bg="#D9D9D9" color="muted.300" shadow="0" mb="3">
-                        <Text fontWeight="300" fontSize="12">1- PC/PC</Text>
-                        <Text fontWeight="300" fontSize="12">4- PC/BX</Text>
-                        <Text fontWeight="300" fontSize="12">144- PC/SKD</Text>
+                    <Box rounded="4" py="2" alignItems="center" justifyContent="center"  bg="text.50" color="text.500" shadow="4" mb="3">
+                        <Text fontWeight="400" fontSize="12">1- PC/PC</Text>
+                        <Text fontWeight="400" fontSize="12">4- PC/BX</Text>
+                        <Text fontWeight="400" fontSize="12">144- PC/SKD</Text>
                     </Box>
 
                     <Box justifyContent="center" alignItems="center" mb="3">
