5c4d4d2b9408aac3589bc0c190f1bb63365077b6
Donz <donnygapulao@gmail.com>
2022-10-27 18:57:48 +0800

    typo fixes

--- components/navigations/AppDraweMenuItem.js
+++ components/navigations/AppDraweMenuItem.js
@@ -6,7 +6,7 @@ const AppDrawerMenuItem = (props) =>{
             bg: "muted.50"
         }} onPress={props.onPress}>
             <HStack alignItems="center" space="5">
-                <Icon size="lg" color="muted.500"  name={props.IconName} as={props.iconAs}/>
+                <Icon size="lg" color="muted.500"  name={props.iconName} as={props.iconAs}/>
                 <Text color="secondary.900" fontSize="14">{props.name}</Text>
             </HStack>
         </Pressable>
