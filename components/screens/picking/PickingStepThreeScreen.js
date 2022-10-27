fa6d6423f8d2e604c01656f379b8e0ddaf07b812
Donz <donnygapulao@gmail.com>
2022-10-27 14:11:07 +0800

    replaced with tile info box

--- components/screens/picking/PickingStepThreeScreen.js
+++ components/screens/picking/PickingStepThreeScreen.js
@@ -6,6 +6,7 @@ import ListItemBox from "../../shared/ListItemBox";
 import AppStyles  from "../../../AppStyles";
 import WhiteButton from "../../shared/WhiteButton";
 import AppBackNavigation from "../../shared/AppBackNavigation";
+import TileInfoBox from "../../shared/TileInfoBox";
 
 export default (props) => {
 
@@ -23,7 +24,7 @@ export default (props) => {
              <Box style={styles.topContainer}>
                     <Text color="tertiary.500" fontSize="12">STEP 3</Text>
                     <Heading size="md" color="tertiary.700" >Enter or Scan Pallet ID</Heading>
-                    <Box p="4" mt="5" bg="muted.50" rounded="4" shadow="5">
+                    <Box p="4" mt="5" bg="text.50" rounded="4" shadow="5">
                         <Text fontSize="12">
                             Item Code : <Text fontWeight="700">MP.001.0150</Text>
                         </Text>
@@ -33,18 +34,9 @@ export default (props) => {
                     </Box>
 
                     <HStack justifyContent="center" space="5" mt="4">
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">83</Text>
-                            <Text color="tertiary.700" fontWeight="700">Ordered</Text>
-                        </Box>
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">83</Text>
-                            <Text color="tertiary.700" fontWeight="700">Remaining</Text>
-                        </Box>
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">PC</Text>
-                            <Text color="tertiary.700" fontWeight="700">UoM</Text>
-                        </Box>
+                        <TileInfoBox title="83" subTitle="Ordered"/>
+                        <TileInfoBox title="83" subTitle="Remaining"/>
+                        <TileInfoBox title="PC" subTitle="UoM"/>
                     </HStack>
 
                     <Box mt="4">
