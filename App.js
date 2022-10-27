efeccc0d0dd3d524bfda126c6e1313fce90aef95
Donz <donnygapulao@gmail.com>
2022-10-27 18:21:41 +0800

    code cleanup

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import 'react-native-gesture-handler';
 import React from "react";
-
+import 'react-native-gesture-handler';
 import {
     NativeBaseProvider,
     extendTheme
 } from "native-base";
 
 import AppTheme from "./AppTheme";
-
 import { NavigationContainer } from '@react-navigation/native';
 import MainDrawer from "./components/navigations/MainDrawer";
 
@@ -15,7 +13,6 @@ import MainDrawer from "./components/navigations/MainDrawer";
 export default function App() {
   // extend the theme
   const theme = extendTheme(AppTheme);
-
 
   return (
         <NativeBaseProvider theme={theme}>
