2a299acfabba3e43f053e4d950f6c7e26eeb70fb
Donz <donnygapulao@gmail.com>
2022-10-27 14:21:31 +0800

    updated style

--- components/screens/picking/PickedItemInfoScreen.js
+++ components/screens/picking/PickedItemInfoScreen.js
@@ -5,6 +5,7 @@ import AppNavigation from "../../shared/AppNavigation";
 import AppStyles  from "../../../AppStyles";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import AppBackNavigation from "../../shared/AppBackNavigation";
+import TileInfoBox from "../../shared/TileInfoBox";
 
 export default (props) => {
 
@@ -13,7 +14,7 @@ export default (props) => {
             <AppBackNavigation navigation={props.navigation} />
              <Box style={styles.topContainer}>
                     <Heading size="md" color="tertiary.700" >Picked Item Information</Heading>
-                    <Box p="4" mt="5" bg="muted.50" rounded="4" shadow="5">
+                    <Box p="4" mt="5" bg="text.50" rounded="4" shadow="5">
                         <Text fontSize="12">
                             Item Code : <Text fontWeight="700">MP.001.0150</Text>
                         </Text>
@@ -23,25 +24,16 @@ export default (props) => {
                     </Box>
 
                     <HStack justifyContent="center" space="5" mt="4">
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">83</Text>
-                            <Text color="tertiary.700" fontWeight="700">Ordered</Text>
-                        </Box>
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">0</Text>
-                            <Text color="tertiary.700" fontWeight="700">Remaining</Text>
-                        </Box>
-                        <Box w="27%" py="2" justifyContent="center" alignItems="center" borderWidth="2" borderColor="text.400" bg="#D9D9D9" rounded="4">
-                            <Text fontWeight="700" fontSize="24">PC</Text>
-                            <Text color="tertiary.700" fontWeight="700">UoM</Text>
-                        </Box>
+                        <TileInfoBox title="83" subTitle="Ordered"/>
+                        <TileInfoBox title="0" subTitle="Remaining"/>
+                        <TileInfoBox title="PC" subTitle="UoM"/>
                     </HStack>
 
                     <Text fontSize="12" fontWeight="700" my="4">QUANTITY PICKED</Text>
 
                     <HStack alignItems="center" rounded="4" bg="white" px="4" py="1" h="90" shadow="5" mb="1">
                         <Box>
-                            <Text fontWeight="300" fontSize="12" pr="10">
+                            <Text fontWeight="400" color="text.600" fontSize="12" pr="10">
                                 19 PC at BRUNEL,R19.S03 by DEFAULT ADMIN on Oct 13,2022 1:56 AM
                             </Text>
                         </Box>
@@ -56,7 +48,7 @@ export default (props) => {
 
                      <HStack alignItems="center" rounded="4" bg="white" px="4" py="1" h="90" shadow="5" mb="1">
                          <Box>
-                             <Text fontWeight="300" fontSize="12" pr="10">
+                             <Text fontWeight="400" color="text.600" fontSize="12" pr="10">
                                  19 PC at BRUNEL,R19.S03 by DEFAULT ADMIN on Oct 13,2022 1:56 AM
                              </Text>
                          </Box>
