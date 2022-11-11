a38bd07b158d96247d0bda2c2f288a2ef2bff80d
Donz <donnygapulao@gmail.com>
2022-11-11 19:35:38 +0800

    renamed dir and file

--- utils/tsApi.js
+++ utils/tsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const useApi = () => {
+const tsApi = () => {
     // Refactor. Move setting to dotenv and retrieve token from secure storage
     const baseURL = 'https://apidev.vogroup.ca/tswarehouseapi';
     const token = 'AAv6mMcToQt8KVH8C7RcOsBtQ31Ml8mITRcHNGhM2A7A+Xaxfc1AeoZcYPU+ETgKahUPSCefJYSvUZP6UxvESM3VMvn6EbKeEPEMyFmj3tDRLxbPEgw54T\\/MXxlpde9JbJeYKj0HzFX25QGylaa48Nb+qzrU+JYGWXQt2R2xlL4NJ9WEe2ONmZcCBtR3jg+3b\\/XwdcSbe8JcLzKI7Gkx8uTSGjD\\/4VaGmqLMN6uxK0i2FdJ+O0\\/9cPwszjZzgIZr3lUKc1H0rpH+D22grrlNdrsnfSrGQJtxJXV6dRQwSd8alwM+b5UeXpVGS96akus9vk93pT6z4\\/i2Qf63brAo\\/g==';
@@ -15,6 +15,7 @@ const useApi = () => {
     });
 
     const tsQuery = (query, variables ='') => {
+        console.log(query);
         return api.post(baseURL,{
             query: `query ${query}`,
             variables: `${variables}`
@@ -32,4 +33,4 @@ const useApi = () => {
 }
 
 
-export default useApi;
+export default tsApi;
