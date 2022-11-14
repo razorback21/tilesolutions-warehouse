8d7d30af1f24f16eeee5afb9a69faefbc259d2f4
Donz <donnygapulao@gmail.com>
2022-11-14 19:28:30 +0800

    removed context code

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import {
 } from "native-base";
 
 import AppTheme from "./AppTheme";
-import AppStore, {AppContext} from "./components/AppStore";
 import {NavigationContainer} from '@react-navigation/native';
 import MainDrawer from "./components/navigations/MainDrawer";
 import LoginScreen from "./components/screens/LoginScreen";
@@ -16,15 +15,13 @@ import LoginScreen from "./components/screens/LoginScreen";
 export default function App() {
   // extend the theme
   const theme = extendTheme(AppTheme);
-  console.log(React.useContext(AppContext));
+
   return (
-      <AppStore>
         <NativeBaseProvider theme={theme}>
             <NavigationContainer>
                 {false ? <MainDrawer/> : <LoginScreen/>}
             </NavigationContainer>
         </NativeBaseProvider>
-      </AppStore>
   );
 }
 
