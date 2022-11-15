995b91f3d6362c96d1d9d9c30e1e8928fdbdf656
Donz <donnygapulao@gmail.com>
2022-11-15 16:47:57 +0800

    added use api hooks

--- hooks/useApi.js
+++ hooks/useApi.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const tsApi = () => {
+const useApi = () => {
     // Refactor. Move setting to dotenv and retrieve token from secure storage
     const baseURL = 'https://apidev.vogroup.ca/tswarehouseapi';
     const token = 'AAv6mMcToQt8KVH8C7RcOsBtQ31Ml8mITRcHNGhM2A7A+Xaxfc1AeoZcYPU+ETgKahUPSCefJYSvUZP6UxvESM3VMvn6EbKeEPEMyFmj3tDRLxbPEgw54T\\/MXxlpde9JbJeYKj0HzFX25QGylaa48Nb+qzrU+JYGWXQt2R2xlL4NJ9WEe2ONmZcCBtR3jg+3b\\/XwdcSbe8JcLzKI7Gkx8uTSGjD\\/4VaGmqLMN6uxK0i2FdJ+O0\\/9cPwszjZzgIZr3lUKc1H0rpH+D22grrlNdrsnfSrGQJtxJXV6dRQwSd8alwM+b5UeXpVGS96akus9vk93pT6z4\\/i2Qf63brAo\\/g==';
+
 
     const api = axios.create({
         baseURl: baseURL,
@@ -11,6 +12,16 @@ const tsApi = () => {
             'Content-Type': 'application/json',
             'Authorization': "Bearer " + token
         },
+    });
+
+    api.interceptors.response.use(function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+    }, function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
     });
 
     const tsQuery = (query, variables ='') => {
@@ -30,4 +41,4 @@ const tsApi = () => {
     return [tsQuery, tsMutation, api]
 }
 
-export default tsApi;
+export default useApi;
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import {AppProviderContext} from "../app/AppProvider";
+import useApi from "./useApi";
+import * as SecureStore from 'expo-secure-store';
 
 const useAuth = () => {
+    const [tsQuery] = useApi();
     const {authenticated, setAuthenticated} = React.useContext(AppProviderContext)
 
-    const appLogin = () => {
-        setAuthenticated(true);
+    const appLogin = (email, password) => {
+        console.log(email, password);
+        //setAuthenticated(true);
+        tsQuery(`
+            AppLogin($Email: String!, $Password: String!) {
+                Login(Email: $Email, Password: $Password) {
+                    success
+                    message
+                    expires
+                    token
+                }
+            }
+        `, `
+            {
+                "Email": "${email}",
+                "Password" : "${password}"
+            }
+        `).then(res => {
+            console.log(res.data)
+        })
     }
 
     const appLogout = () => {
