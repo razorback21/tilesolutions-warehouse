802d1f63b6e2388653b2a6deece8870e8535ca59
Donz <donnygapulao@gmail.com>
2022-11-15 17:31:24 +0800

    added app logout

--- components/shared/AppNavMoreMenu.js
+++ components/shared/AppNavMoreMenu.js
@@ -1,18 +1,16 @@
 import {Box, Menu, Icon, IconButton} from 'native-base';
 import Ionicons from "@expo/vector-icons/Ionicons";
-
+import useAuth from "../../hooks/useAuth";
 
 const AppNavMoreMenu = () => {
-    const logout = () => {
-        console.warn('log out')
-    }
+    const [appLogout] = useAuth();
 
     return (<Box w="5%" alignItems="center">
                 <Menu on w="150" defaultIsOpen={false} trigger={triggerProps => {
                        return <IconButton {...triggerProps} icon={<Icon as={Ionicons} name='md-ellipsis-vertical' size='5'/>}
                                          _icon={{color: "white"}}/>;
                 }}>
-                    <Menu.Item onPress={logout}>Log out</Menu.Item>
+                    <Menu.Item onPress={appLogout}>Log out</Menu.Item>
                 </Menu>
             </Box>
     )
