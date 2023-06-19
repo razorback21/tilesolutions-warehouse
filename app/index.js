6875297cc98856bd4ccfec333992c17f2bb2c86a
Donz <donnygapulao@gmail.com>
2023-06-19 17:30:27 +0800

    index initial code

--- app/index.js
+++ app/index.js
@@ -4,14 +4,14 @@ import {AppProviderContext} from "../components/AppProvider";
 import AppDrawer from "../components/shared/AppDrawer";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 // Screens
-import LoginScreen from "../components/LoginScreen";
+import LoginScreen from "../components/screens/LoginScreen";
 import DashboardScreen from "../components/screens/DashboardScreen";
 
 export default function Index () {
-    const {authenticated, setAuthenticated} =  React.useContext(AppProviderContext);
+    const {authenticated} =  React.useContext(AppProviderContext);
     const Drawer = createDrawerNavigator();
 
-    return authenticated ? <LoginScreen /> : (
+    return !authenticated ? <LoginScreen /> : (
         <Drawer.Navigator
             initialRouteName="index" screenOptions={{
             headerShown: false,
