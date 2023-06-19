38068b1835c9957e32b24389038bd2295eebb814
Donz <donnygapulao@gmail.com>
2023-06-19 18:06:57 +0800

    initial login screen code

--- components/screens/LoginScreen.js
+++ components/screens/LoginScreen.js
@@ -1,23 +1,54 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { StyleSheet } from 'react-native';
-import {Box, Button, Input, VStack, Text, Heading, Icon, Stack} from 'native-base';
+import {Box, Button, Input, VStack, Text, Heading, Icon, Stack, useDisclose} from 'native-base';
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import ModalMessage from "../shared/ModalMessage";
 import useAuth from "../../hooks/useAuth";
+import useApi from "../../hooks/useApi";
 
 export default () => {
-    const [appLogin] = useAuth();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [appLogin, appLogout] = useAuth();
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    const [modalTitle, setModalTitle ] = useState('Message');
+    const [modalMessage, setModalMessage ] = useState('');
 
-    const emailInputHandler = (text) => {
-        setEmail(text)
+
+    const {
+        isOpen,
+        onOpen,
+        onClose
+    } = useDisclose();
+
+    const [ tsQuery ] = useApi();
+    const handleEmailInput = (text) => {
+        setEmailValue(text);
     }
 
-    const passwordInputHandler = (text) => {
-        setPassword(text)
+    const handlePasswordInput = (text) => {
+        setPasswordValue(text);
     }
 
-    return (<VStack style={styles.topContainer} justifyContent="space-between">
+    const handleLogin = () => {
+        if(!emailValue.length || !passwordValue.length) {
+            setModalTitle('Error');
+            setModalMessage('Email and Password field is required.');
+            onOpen();
+        } else {
+            appLogin(emailValue, passwordValue).catch(error => {
+                setModalTitle('Access Denied');
+                setModalMessage('Invalid credentials. Try again.')
+                onOpen();
+                appLogout();
+            });
+        }
+
+    }
+
+    return (
+        <>
+        <ModalMessage isOpen={isOpen} onClose={onClose} title={modalTitle} message={modalMessage}/>
+        <VStack style={styles.topContainer} justifyContent="space-between">
         <Box h="300" bg="red.00" justifyContent="center" alignItems="center">
             <Icon name="warehouse" as={FontAwesome5} color="#ffffff" size="250" style={{textShadowOffset:{width: 0, height:5}, textShadowRadius:1, textShadowColor:'rgba(0, 0, 0, 0.25)'}} position="absolute" right="0" bottom="0"/>
             <Stack mt="-100">
@@ -26,11 +57,12 @@ export default () => {
             </Stack>
         </Box>
         <Box px="5" mt="40%" style={styles.loginBox}>
-            <Input size="lg" mb="2" placeholder="Email" onChangeText={emailInputHandler}/>
-            <Input size="lg" mb="2" placeholder="Password" onChangeText={passwordInputHandler} type="password"/>
-            <Button onPress={() => appLogin(email, password)}>Log In</Button>
+            <Input size="lg" mb="2" placeholder="Email" onChangeText={handleEmailInput} />
+            <Input size="lg" mb="2" placeholder="Password" type="password" onChangeText={handlePasswordInput}/>
+            <Button onPress={handleLogin}>Log In</Button>
         </Box>
-    </VStack>);
+    </VStack>
+    </>);
 }
 
 const styles = StyleSheet.create({
