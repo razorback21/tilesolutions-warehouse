21f3ec252a6190d7b9fe375f488ebac82faa48e3
Donz <donnygapulao@gmail.com>
2023-06-20 14:39:01 +0800

    fix useAuth destructuring

--- components/shared/AppNavMoreMenu.js
+++ components/shared/AppNavMoreMenu.js
@@ -3,14 +3,18 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import useAuth from "../../hooks/useAuth";
 
 const AppNavMoreMenu = () => {
-    const [appLogout] = useAuth();
+    const {appLogout} = useAuth();
+
+    const handleLogout = () => {
+        appLogout();
+    }
 
     return (<Box w="5%" alignItems="center">
                 <Menu on w="150" defaultIsOpen={false} trigger={triggerProps => {
                        return <IconButton {...triggerProps} icon={<Icon as={Ionicons} name='md-ellipsis-vertical' size='5'/>}
                                          _icon={{color: "white"}}/>;
                 }}>
-                    <Menu.Item onPress={appLogout}>Log out</Menu.Item>
+                    <Menu.Item onPress={handleLogout}>Log out</Menu.Item>
                 </Menu>
             </Box>
     )
