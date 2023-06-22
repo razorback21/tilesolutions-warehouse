942e065248a2fead1e77241ca95c76670d72af1c
Donz <donnygapulao@gmail.com>
2023-06-22 18:11:58 +0800

    added react query client provider

--- components/AppProvider.js
+++ components/AppProvider.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+import {QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const AppProviderContext = React.createContext();
+const MyQueryClient = new QueryClient();
 
 const AppProvider = (props) => {
+
     const [authenticated, setAuthenticated] = React.useState(false)
-    return (<AppProviderContext.Provider value={{authenticated, setAuthenticated}}>
-        {props.children}
-    </AppProviderContext.Provider>)
+    return (<QueryClientProvider client={MyQueryClient}>
+                <AppProviderContext.Provider value={{authenticated, setAuthenticated}}>
+                    {props.children}
+                </AppProviderContext.Provider>
+        </QueryClientProvider>
+    )
 }
 
 export default AppProvider;
-export {AppProviderContext};
+export {AppProviderContext, MyQueryClient};
