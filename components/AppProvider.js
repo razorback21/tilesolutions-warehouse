723a5e8886851d802bdfa00e77e79d668bf40efc
Donz <donnygapulao@gmail.com>
2023-06-23 17:17:20 +0800

    revertback to old code

--- components/AppProvider.js
+++ components/AppProvider.js
@@ -1,24 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import {QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const AppProviderContext = React.createContext();
 const MyQueryClient = new QueryClient();
 import * as SecureStore from 'expo-secure-store';
 
 const AppProvider = (props) => {
-
-    const isAuthenticated = async () => {
-        try {
-            const token = await SecureStore.getItemAsync('api-key');
-            return token ? true : false;
-        } catch (e) {
-            console.log('isAuthenticated token read error', e);
-        }
-
-        return false;
-    }
+    const [authenticated, setAuthenticated] = useState(false);
 
     return (<QueryClientProvider client={MyQueryClient}>
-                <AppProviderContext.Provider value={{isAuthenticated}}>
+                <AppProviderContext.Provider value={{authenticated, setAuthenticated}}>
                     {props.children}
                 </AppProviderContext.Provider>
         </QueryClientProvider>
