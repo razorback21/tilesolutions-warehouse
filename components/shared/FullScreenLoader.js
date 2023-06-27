43a6f9313681f20d90b75fda062ea437d56e5028
Donz <donnygapulao@gmail.com>
2023-06-27 15:40:35 +0800

    added header

--- components/shared/FullScreenLoader.js
+++ components/shared/FullScreenLoader.js
@@ -1,8 +1,17 @@
-import {Spinner, VStack, Center, Text, Box} from 'native-base';
+import {Spinner, VStack, Center, Text, Box, HStack, IconButton, Icon} from 'native-base';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AppNavMoreMenu from "./AppNavMoreMenu";
+import React from "react";
 
 const FullScreenLoader = (props) => {
-    return (
-        <VStack justifyContent="center" alignItems="center" width="100%" height="100%">
+    return (<>
+                <Box safeAreaTop bg="primary.500"/>
+                <HStack bg="primary.500" px="1" pt="0" pb="1" justifyContent="center" alignItems="center" w="100%">
+                    <Text color="white" fontSize="16" fontWeight="bold">
+                        {props.title}
+                    </Text>
+                </HStack>
+                <VStack justifyContent="center" alignItems="center" width="100%" height="100%">
                 <Box>
                     <Center>
                         <Spinner size={props.size} color={props.color}/>
@@ -11,7 +20,8 @@ const FullScreenLoader = (props) => {
                         <Text>{props.text}</Text>
                     </Center>
                 </Box>
-        </VStack>
+            </VStack>
+        </>
     )
 }
 
@@ -20,5 +30,6 @@ export default FullScreenLoader;
 FullScreenLoader.defaultProps = {
     size: 'md',
     color: 'primary.500',
-    text: 'Loading...'
+    text: 'Loading...',
+    title: 'Please wait...'
 }
