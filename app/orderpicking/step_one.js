ca92e841671793c7069540c1e20674a5726f0745
Donz <donnygapulao@gmail.com>
2023-06-27 14:47:04 +0800

    step one initial code

--- app/orderpicking/step_one.js
+++ app/orderpicking/step_one.js
@@ -4,48 +4,87 @@ import {Box, Text, Heading, ScrollView } from "native-base";
 import ListItemBox from "../../components/shared/ListItemBox";
 import AppStyles from "../../AppStyles";
 import AppBackNavigation from "../../components/shared/AppBackNavigation";
+import {useQuery} from "@tanstack/react-query";
+import {useLocalSearchParams} from "expo-router";
+import useApi from "../../hooks/useApi";
+import SpinnerModal from "../../components/shared/SpinnerLoader";
 
 
 export default (props) => {
+    const params = useLocalSearchParams();
+    const {tsQuery} = useApi();
 
-    const ItemContent = () => {
+    const orderItems = async (co_number)=> {
+        return await tsQuery(
+            `
+                PickingOrderedItems($CONumber: String!) {
+                    PickingOrderedItems(CONumber: $CONumber) {
+                        Status
+                        WrittenBy
+                        Customer
+                        CONumber
+                        OrderItems {
+                            Code
+                            Description
+                            Shade
+                            Qty
+                        }
+                    }
+                }
+            `,
+            {
+                CONumber: co_number
+            }
+        ).then(res => {
+            return res.data.data.PickingOrderedItems
+        })
+    }
+
+    const orderItemsQuery = useQuery({
+        queryKey: ["ordered-items", params.co],
+        queryFn: () => orderItems(params.co)
+    })
+
+    const ItemContent = ({data}) => {
         return (
             <>
                 <Box mb="1">
-                    <Text fontWeight="700" fontSize="12" color="primary.600">AA.911.2424   <Box h="2.5" width="2.5" bg="success.500" rounded="full" mt="0.2"></Box></Text>
+                    <Text fontWeight="700" fontSize="12" color="primary.600">{data.Code}   <Box h="2.5" width="2.5" bg="success.500" rounded="full" mt="0.2"></Box></Text>
                 </Box>
                 <Box mb="1">
-                    <Text fontWeight="400" fontSize="12" color="text.600">La Marca Statuarietto Polished Rectified Glazed Porcelain 24" x 24"</Text>
+                    <Text fontWeight="400" fontSize="12" color="text.600">{data.Description}</Text>
                 </Box>
                 <Box>
-                    <Text fontWeight="400" fontSize="12" color="text.600">Order Qty : 83 PC</Text>
+                    <Text fontWeight="400" fontSize="12" color="text.600">Order Qty : {data.Qty}</Text>
                 </Box>
             </>
         );
     }
 
+    if(orderItemsQuery.status === 'loading') {
+        return <SpinnerModal isOpen={true} size="lg"/>
+    }
+
     return (
         <>
-            <AppBackNavigation navigation={props.navigation} />
+            <AppBackNavigation path="/orderpicking/order_received" />
             <Box style={styles.topContainerNoFlex}>
                 <Text color="tertiary.500" fontSize="12">STEP 1</Text>
                 <Heading size="md" color="tertiary.700">Picking order for pickup</Heading>
 
-                <Text my="2" fontSize="12" color="text.600">
-                    Written by: Philiipine{"\n"}
-                    Sold to: JS Renovations{"\n"}
-                    For C.O: <Text fontWeight="700">000183381</Text>
+                <Text mt="1" fontSize="12" color="text.600">
+                    For C.O: <Text fontWeight="700">{orderItemsQuery.data.CONumber}</Text> - Written by: {orderItemsQuery.data.WrittenBy}{"\n"}
+                    Sold to: {orderItemsQuery.data.Customer}
                 </Text>
             </Box>
             <Box style={styles.contentContainer}>
                 <Box style={styles.innerBox} bg={"tertiary.200"}>
                     <ScrollView>
-                        <ListItemBox content={<ItemContent />}/>
-                        <ListItemBox content={<ItemContent />}/>
-                        <ListItemBox content={<ItemContent />}/>
-                        <ListItemBox content={<ItemContent />}/>
-                        <ListItemBox content={<ItemContent />}/>
-                        <ListItemBox content={<ItemContent />}/>
+                        {
+                            orderItemsQuery.isSuccess && orderItemsQuery.data.OrderItems.map((item) => {
+                               return  <ListItemBox key={item.CONumber} content={<ItemContent data={item}/>}/>
+                            })
+                        }
                     </ScrollView>
                 </Box>
             </Box>
