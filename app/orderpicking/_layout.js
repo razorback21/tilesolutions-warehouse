ee11581548be7bcb8b0bc54908cc98e2c4776c1b
Donz <donnygapulao@gmail.com>
2023-06-30 17:51:42 +0800

    added layout

---
+++ app/orderpicking/_layout.js
@@ -0,0 +1,11 @@
+import {Stack} from "expo-router";
+import React from "react";
+
+export default (props) => {
+    return (
+        <Stack screenOptions={{
+            headerShown: false,
+        }}>
+        </Stack>
+    )
+}
