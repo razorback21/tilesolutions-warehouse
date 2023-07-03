3853629c25cdcdeee4ff5ae3560fb81716d7654f
Donz <donnygapulao@gmail.com>
2023-07-03 15:04:16 +0800

    added orderpicking_queries.js to hold common queries for order picking section

---
+++ queries/orderpicking_queries.js
@@ -0,0 +1,26 @@
+// Put all reusable graphql queries here
+import useApi from "../hooks/useApi";
+
+const {tsQuery} = useApi();
+
+export const fetchPickItemData =  (sales_item_id) => {
+    return tsQuery(`
+            PickItemData($SalesItemID: Int!) {
+              PickItemData(SalesItemID: $SalesItemID) {
+                  Ordered
+                  ProductID
+                  ProductCode
+                  ProductDescription
+                  RemainingToBePick
+                  UoM
+              }
+            }
+        `,
+
+        {
+            "SalesItemID": sales_item_id
+        }).then(res => {
+        return res.data.data.PickItemData
+    })
+}
+
