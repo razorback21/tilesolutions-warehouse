4e44e61316604b8d913c0e05eaa91c8be0604e9c
Donz <donnygapulao@gmail.com>
2023-07-11 14:28:22 +0800

    fixed styling

--- components/shared/WhiteButton.js
+++ components/shared/WhiteButton.js
@@ -1,4 +1,4 @@
-import {Button, Icon, Stack, Text} from "native-base";
+import {Button, Icon, Box, Text} from "native-base";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React from "react";
 
@@ -6,12 +6,19 @@ import React from "react";
 export default (props) => {
     return (
         <>
-            <Button onPress={props.onPress} variant="whiteButton" leftIcon={<Icon as={MaterialIcons} name={props.icon} size="md" color="text.500"/>} mb={props.mb} mt={props.mt}>
-                <Stack direction="row" >
-                    <Text w="80%" fontSize="14" ml="1">{props.title}</Text>
-                    <Icon position="absolute" right="-55" top="1.5" size="xs" as={MaterialIcons} name="arrow-forward-ios" color="primary.500"/>
-                </Stack>
-            </Button>
+            <Box style={{position:"relative"}}>
+                <Button onPress={props.onPress}
+                        variant="whiteButton"
+                        leftIcon={<Icon as={MaterialIcons} name={props.icon} size="md" color="text.500"/>}
+                        mb={props.mb}
+                        mt={props.mt}
+                        w="100%"
+                >
+                    <Text w="80%" fontSize="14" ml="1" width="100%">{props.title}</Text>
+                </Button>
+                <Icon position="absolute" right="5" top="15px" size="xs" as={MaterialIcons} name="arrow-forward-ios" color="primary.500"/>
+            </Box>
+
         </>
     )
 }
