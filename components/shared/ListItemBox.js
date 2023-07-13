5489848f672a73fabcaa687695c3ce04bc10bceb
Donz <donnygapulao@gmail.com>
2023-07-13 15:34:45 +0800

    added status indicator

--- components/shared/ListItemBox.js
+++ components/shared/ListItemBox.js
@@ -3,9 +3,26 @@ import {HStack, Stack, Icon, Flex, Pressable, Box } from "native-base";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const ListItemBox = (props) => {
+    console.log(props.statusColor);
     return <Pressable _pressed={{
         bg: "text.50"
     }} bg="#FFFFFF"  mb={props.marginBottom} onPress={props.onPress} rounded="5" mx="3" shadow="2">
+        {
+            props.statusColor && <><Box style={{
+                position: "absolute",
+                right: 0,
+                width: '0px',
+                height: '0px',
+                borderTopWidth:'20px',
+                borderTopStyle: "solid",
+                borderLeftWidth: "20px",
+                borderLeftStyle: "solid",
+                borderLeftColor: "transparent"}}
+                roundedTopRight="5"
+                borderColor={props.statusColor}
+            ></Box></>
+        }
+
         <HStack px="4" py="2.5" alignItems="center">
             <Box w={props.showRightIcon ? "95%" : "100%"}>
                 {props.content}
@@ -26,7 +43,8 @@ ListItemBox.defaultProps = {
     marginBottom: 2,
     rightIcon: "arrow-forward-ios",
     rightIconSize: "sm",
-    showRightIcon: true
+    showRightIcon: true,
+    statusColor: "#fff"
 }
 
 export default ListItemBox;
