002a9bcc5fd81d4d1beaba4c454ef45707a67c78
Donz <donnygapulao@gmail.com>
2023-08-02 14:53:17 +0800

    Shared BarcodeScanner  component

---
+++ components/shared/AppBarCodeScanner.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function(props) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+
+        getBarCodeScannerPermissions();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        props.codeHandler(data);
+    };
+
+    if (hasPermission === null) {
+        return <View style={styles.container}><Text>Requesting for camera permission</Text></View>;
+    }
+    if (hasPermission === false) {
+        return <View style={styles.container}><Text>No access to camera</Text></View>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
